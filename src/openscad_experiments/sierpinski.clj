c53bdd1dad96a6620aa1a08edadcbad7e8c38e1d
Stian Eikeland <stian@eikeland.se>
2019-02-05 01:22:37 +0100

    Sierpinksi

---
+++ src/openscad_experiments/sierpinski.clj
@@ -0,0 +1,30 @@
+(ns openscad-experiments.sierpinski
+  (:require [scad-clj.model :as m]
+            [openscad-experiments.helpers :refer [render]]))
+
+(def tetra-points [[(/ 1 (Math/sqrt 3)), 0, 0]
+                   [(/ -1 (* 2 (Math/sqrt 3))), 1/2, 0]
+                   [(/ -1 (* 2 (Math/sqrt 3))), -1/2, 0]
+                   [0, 0, (Math/sqrt 2/3)]])
+
+(defn tetra []
+  (m/polyhedron tetra-points [[0, 1, 2]
+                              [2, 1, 3]
+                              [0, 2, 3]
+                              [0, 3, 1]]))
+
+(defn- level-scale [level]
+  (Math/pow 2 (dec level)))
+
+(defn- scale-point [point level]
+  (let [scale (level-scale level)]
+    (map (partial * scale) point)))
+
+(defn sierpinski [level]
+  (if (zero? level)
+    (tetra)
+    (let [scales   (map #(scale-point % level) tetra-points)
+          children (map #(m/translate % (sierpinski (dec level))) scales)]
+      (apply m/union children))))
+
+(render (sierpinski 5))
