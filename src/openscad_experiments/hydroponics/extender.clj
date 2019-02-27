6a35b30c7ecd9015521c60e1e5a876c24238f283
Stian Eikeland <stian@eikeland.se>
2019-02-28 00:55:48 +0100

    Extender for ikea hydroponics

---
+++ src/openscad_experiments/hydroponics/extender.clj
@@ -0,0 +1,23 @@
+(ns openscad-experiments.hydroponics.extender
+  (:require [scad-clj.model :as m]
+            [openscad-experiments.helpers :refer [render]]))
+
+(def opening-diameter 55)
+(def outside-diameter 60)
+(def curve-radius 10)
+(def extender-height 20)
+
+(defn round-shape [diameter]
+  (let [circle (m/circle curve-radius)
+        circles (for [x [-1 1]
+                      y [-1 1]]
+                  (as-> [x y 0] $
+                      (map (partial * (- (/ diameter 2) curve-radius)) $)
+                      (m/translate $ circle)))]
+    (apply m/hull circles)))
+
+(render (->> (m/difference (round-shape outside-diameter)
+                           (round-shape opening-diameter))
+             (m/extrude-linear {:height extender-height
+                                :center true
+                                :scale 0.9})))
